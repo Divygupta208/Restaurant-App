06d6582c213efc4ea78dc2e2025bda9e18879a66
Divy Gupta <divygupta208@gmail.com>
2024-01-31 09:16:40 +0530

    Cart and Modal components

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,21 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
-
-const MealItemForm = () => {
+import Input from "../../UI/Input";
+const MealItemForm = (props) => {
   return (
-    <form>
-      <div className={classes["form-control"]}>
-        <h3>Amount</h3>
-        <input type="number" id="input-number" placeholder="1" />
-      </div>
-      <button className={classes["form-button"]}>
-        <h3>+Add</h3>
-      </button>
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
     </form>
   );
 };
